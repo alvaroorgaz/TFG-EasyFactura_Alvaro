58891e601b846a2a2e42ac00b9754c065f3912f5
alvaroorgaz <alvaro.orgazmonleon24@campusfp.es>
2026-04-16 18:09:53 +0200

    Abrir securityconfig
    He abierto el security config temporalmente para entrar en la web, para hacer los demas crud hasta que empiece a hacer el login

--- TFG/src/main/java/com/example/TFG/security/SecurityConfig.java
+++ TFG/src/main/java/com/example/TFG/security/SecurityConfig.java
@@ -3,30 +3,19 @@ package com.example.TFG.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http ) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login","/css/**").permitAll()
-                .requestMatchers("/empresa/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable());
 
-        )
-        .formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/home", true)
-                .permitAll()
-        )
-                .logout(LogoutConfigurer::permitAll);
         return http.build();
-
     }
 }
